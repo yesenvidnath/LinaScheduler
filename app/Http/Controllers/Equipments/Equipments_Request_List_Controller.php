c32801163e4be8b62d355bed6e7338b2c789e7bd
yesenvidnath <ykandalama@gmail.com>
2025-04-11 23:12:52 +0530

    The controller was cteated for the Equipment request list

---
+++ app/Http/Controllers/Equipments/Equipments_Request_List_Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Equipments;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Equipments_Request_List_Controller extends Controller
+{
+    //
+}
