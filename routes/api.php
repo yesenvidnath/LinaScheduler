2b3326c03ee572fe5dee8f62ab4401d1d68f1f7c
yesenvidnath <ykandalama@gmail.com>
2025-06-11 23:38:23 +0530

    api file was updated

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
